bf3b595c369ab67757f7cfb31e572456010f8358
darkander <kingdarkander@gmail.com>
2021-08-18 15:32:25 +0300

    input logic - way 1

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 export const TodoForm: React.FC = () => {
+    const [title, setTitle] = useState<string>('')
+    const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setTitle(event.target.value)
+    }
+    const keyPressHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            console.log(title);
+            setTitle('')
+        }
+ 
+  
+    }
     return (
     <div className="input-field mt2">
-        <input type="text" id='title' placeholder='Add...' />
+        <input value={title} onChange={changeHandler} 
+        type="text" id='title' placeholder='Add...'
+         onKeyPress={keyPressHandler} />
         <label htmlFor="title" className='active'>
             Add...
         </label>
     </div>
     )
+
 }
